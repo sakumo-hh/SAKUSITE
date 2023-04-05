fe257cdfe2bf13f9f47bc25e2157973756e68352
sakumo-hh <akihiro3399mg@gmail.com>
2023-04-05 18:43:00 +0900

    3rd

--- mysite/views.py
+++ mysite/views.py
@@ -15,36 +15,6 @@ import logging
 import pandas as pd
 #logging.debug('hi_debug')
 
-# consumer_key = '91yx7Z1PzJVyL6PUqv5EwLlFU'
-# consumer_secret= 'ZTW5QFjnEdCSw8PSZvEuBQVCVveIg4OfTl641LHvRAhgwNnd0Z'
-# access_token    = '1376917526316085253-7RN39UcFSRsVT10yYtw5mchPxNGitF'
-# access_secret  = 'JdnoJNUT3Mu7C8ANe7jvtyYZt4TcnfXHcW34eJ7qXXcnR'
-
-# oauth_session_params = {
-#     'consumer_key':'91yx7Z1PzJVyL6PUqv5EwLlFU',
-#     'consumer_secret':'ZTW5QFjnEdCSw8PSZvEuBQVCVveIg4OfTl641LHvRAhgwNnd0Z',
-#     'access_token' : '1376917526316085253-7RN39UcFSRsVT10yYtw5mchPxNGitF',
-#     'access_secret':'JdnoJNUT3Mu7C8ANe7jvtyYZt4TcnfXHcW34eJ7qXXcnR',
-# }
-
-# twitter = OAuth1Session(
-#     oauth_session_params['consumer_key'],
-#     oauth_session_params['consumer_secret'],
-#     oauth_session_params['access_token'],
-#     oauth_session_params['access_secret']
-#     )
-
-# url = "https://api.twitter.com/1.1/search/tweets.json?"
-# params={
-#     'q':'#歌ってみた',
-#     'count': 2,
-# }
-# res=twitter.get(url,params=params)
-
-# print(res.text)
-
-# tweets = 100
-
 def index(request):
     objs = Article.objects.all()
     context = {
